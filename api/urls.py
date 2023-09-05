72f11d2f1b59cfc13f0a7d0cde6253987be82797
Rafael Tomazini <rafaelnt83@gmail.com>
2023-09-05 14:33:15 -0300

    feat: django rest framework working, views, models and urls created

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.getRoutes, name="Routes"),
+    path("", views.getRoutes.as_view(), name="routes"),
+    path("notes/", views.getNotes.as_view(), name="notes"),
+    path("notes/<note_id>/", views.getSingleNote.as_view(), name="note"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,40 +1,57 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Note
+from .serializer import NoteSerializer
 
-# Create your views here.
+
+class getRoutes(APIView):
+    def get(self, request):
+        routes = [
+            {
+                "Endpoint": "/notes/",
+                "method": "GET",
+                "body": None,
+                "description": "Returns an array of notes",
+            },
+            {
+                "Endpoint": "/notes/id",
+                "method": "GET",
+                "body": None,
+                "description": "Returns a single note object",
+            },
+            {
+                "Endpoint": "/notes/create/",
+                "method": "POST",
+                "body": {"body": ""},
+                "description": "Creates new note with data sent in post request",
+            },
+            {
+                "Endpoint": "/notes/id/update/",
+                "method": "PUT",
+                "body": {"body": ""},
+                "description": "Creates an existing note with data sent in post request",
+            },
+            {
+                "Endpoint": "/notes/id/delete/",
+                "method": "DELETE",
+                "body": None,
+                "description": "Deletes and exiting note",
+            },
+        ]
+        return Response(routes)
 
 
-def getRoutes(request):
-    routes = [
-        {
-            "Endpoint": "/notes/",
-            "method": "GET",
-            "body": None,
-            "description": "Returns an array of notes",
-        },
-        {
-            "Endpoint": "/notes/id",
-            "method": "GET",
-            "body": None,
-            "description": "Returns a single note object",
-        },
-        {
-            "Endpoint": "/notes/create/",
-            "method": "POST",
-            "body": {"body": ""},
-            "description": "Creates new note with data sent in post request",
-        },
-        {
-            "Endpoint": "/notes/id/update/",
-            "method": "PUT",
-            "body": {"body": ""},
-            "description": "Creates an existing note with data sent in post request",
-        },
-        {
-            "Endpoint": "/notes/id/delete/",
-            "method": "DELETE",
-            "body": None,
-            "description": "Deletes and exiting note",
-        },
-    ]
-    return JsonResponse(routes, safe=False)
+class getNotes(APIView):
+    def get(self, request):
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+
+class getSingleNote(APIView):
+    def get(self, request, note_id):
+        note = Note.objects.get(pk=note_id)
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
